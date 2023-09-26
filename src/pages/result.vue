5c1f59ec77d7d018843378d43ffb030e49475f78
一只死兔子 <llzz0895@163.com>
2023-09-26 17:30:29 +0800

    feat: 添加gsap

--- src/pages/result.vue
+++ src/pages/result.vue
@@ -1,20 +1,34 @@
 <script setup lang="ts">
+import { gsap } from 'gsap'
 import { answerRecord } from '~/composables'
 
 const score = computed(() => {
   return answerRecord.value.filter(Boolean).length
 })
 
+const d = reactive({
+  nums: 0,
+})
+
 const v = ref(false)
 
 const test = () => {
-  v.value = true
+  // v.value = true
+
+  gsap.to(d, {
+    duration: 3,
+    nums: 70,
+    ease: 'power1.out',
+    onUpdate: () => {
+      d.nums = Math.round(d.nums)
+    },
+  })
 }
 </script>
 
 <template>
   <div :class="{anime: v}">
-    结果 {{ score }}
+    结果 {{ score }} {{ d.nums }}
   </div>
 
   <div @click="test">
